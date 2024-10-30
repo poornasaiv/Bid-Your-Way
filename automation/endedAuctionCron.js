fd7dfa9c1e94966c99173b727ecf700246af53cb
Anvith <anvith.paturu@codinixcloud.com>
2024-10-30 15:57:32 -0400

    changes

---
+++ automation/endedAuctionCron.js
@@ -0,0 +1,6 @@
+import cron from "node-cron";
+import { Auction } from "../models/auctionSchema.js";
+import { User } from "../models/userSchema.js";
+import { Bid } from "../models/bidSchema.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import { calculateCommission } from "../controllers/commissionController.js";
